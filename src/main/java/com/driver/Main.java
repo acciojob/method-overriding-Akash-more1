28cf502df440e66815511e2bdc3e2e1e2a76eda8
Akash-more1 <akashmore9725@gmail.com>
2023-10-13 22:49:57 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,7 @@
 package com.driver;
 
 public class Main {
-  
+  class_B demo=new class_B();
+  String s=demo.meth();
+
 }
---
+++ src/main/java/com/driver/class_B.java
@@ -0,0 +1,8 @@
+package com.driver;
+
+public class class_B extends class_A {
+    @Override
+    public  String meth(){
+        return "Invoking method from class B";
+    }
+}
