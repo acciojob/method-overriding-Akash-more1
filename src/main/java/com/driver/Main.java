92f8910eb84f9827bcebdef4112d6c8f2a262b7b
Akash-more1 <akashmore9725@gmail.com>
2023-10-14 22:49:25 +0530

    second commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,9 @@ package com.driver;
 
 public class Main {
   public static void main(String[] args) {
-    class_B demo=new class_B();
-    String s=demo.meth();
-//    System.out.println(s);
+    B obj=new B();
+    String s=obj.meth();
+   System.out.println(s);
   }
 
 
