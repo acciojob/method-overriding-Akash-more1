c27922049f032e6c27bd8fde9aeab9fb482575b9
Akash-more1 <akashmore9725@gmail.com>
2023-10-15 11:52:33 +0530

    third

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,7 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     B obj=new B();
-//    String s=obj.meth();
+   String s=obj.meth();
    System.out.println(obj.meth());
   }
 
